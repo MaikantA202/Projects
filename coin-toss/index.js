3ce8ed2dd984a2372faafe0b0717d30d8ec6e5b3
Manikanta202 <manikanta202@gmail.com>
2023-06-03 01:44:40 +0530

    commit-2

--- coin-toss/index.js
+++ coin-toss/index.js
@@ -1,6 +1,6 @@
 document.querySelector(".btn").addEventListener("click", function(){
     var a=Math.floor(Math.random()*2)+1;
-    document.querySelector(".img1").setAttribute("src","images/coin-"+a+".png");
+    $(".img1").attr("src","images/coin-"+a+".png");
     if (a===1){
         $("h3").text("Heads");
     }
